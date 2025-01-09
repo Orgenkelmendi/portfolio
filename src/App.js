4a692696fb1f5d5ecba9a84f825886fb4b37626d
orgenkelmendi <okelmendi@gmail.com>
2025-01-08 23:45:59 -0500

    Adding header, landing section and socials.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <h1> Orgen's Porfolio </h1>
+      <div id='socials'>
+        <a href="https://www.linkedin.com/in/orgen-kelmendi" target="_blank">
+          <FontAwesomeIcon id='linkedin' icon={faLinkedin} />
+        </a>
+        <a href="https://github.com/Orgenkelmendi" target="_blank">
+          <FontAwesomeIcon id='github' icon={faGithub} />
+        </a>
+        <a href="https://x.com/kelmendiorgen" target="_blank">
+          <FontAwesomeIcon id='twitter' icon={faXTwitter} />
+        </a>
+      </div>
+      <header>
+        <a>JOURNEY</a>
+        <a>TECH STACK</a>
+        <a>PROJECTS</a>
+        <a>GET IN TOUCH</a>
+      </header>
+      <div id='landing'>
+            <p><span id='frontend'>Front End</span> Developer.</p>
+            <p><span id='tech'>Tech</span> Enthusiast.</p>
+            <p><span id='lifetime'>Lifetime</span> Learner.</p>
+      </div>
     </div>
   );
 }
